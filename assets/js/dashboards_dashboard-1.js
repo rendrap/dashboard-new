7c3e51f1c73ffc4a4aa9cb0407df8ff386930a6e
rendrap <rendrapradana@gmail.com>
2019-12-26 10:49:58 +0700

    add ToDo List

--- assets/js/dashboards_dashboard-1.js
+++ assets/js/dashboards_dashboard-1.js
@@ -12,5 +12,22 @@ $(function() {
       $(this).toggleClass('is_animating');
   });
 
+  // Adjust MyTodos header border visibility on icon-add click
+  $('#collapseTodos').on('show.bs.collapse', function () {
+    $('#todosHeader').addClass('border-0');
+  })
+
+  $('#collapseTodos').on('hide.bs.collapse', function () {
+    $('#todosHeader').removeClass('border-0');
+  })
+
+
+// Drag&Drop
+
+  dragula(Array.prototype.slice.call(document.querySelectorAll('.task-list')), {
+    moves: function (el, container, handle) {
+      return handle.classList.contains('task-list-handle');
+    }
+  });
 
 });
