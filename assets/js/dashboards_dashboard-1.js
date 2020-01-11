b00f6e601246d187441b2f40d0de6cf4808bb67f
rendrap <rendrapradana@gmail.com>
2020-01-11 15:43:31 +0700

    Getting chart.js to consume JSON data from local API

--- assets/js/dashboards_dashboard-1.js
+++ assets/js/dashboards_dashboard-1.js
@@ -35,67 +35,92 @@ $(function() {
     $(this).wrap($('<div style="height:' + this.getAttribute('height') + 'px"></div>'));
   });
 
-  var graphChart = new Chart(document.getElementById('chart-graph').getContext("2d"), {
-    type: 'line',
-    data: {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [{
-        label:           'Missing assets value',
-        data:            [226043, 220000, 250000, 160000, 210000, 219000, 280000],
-        lineTension: 0.4,
-        borderWidth: 2,
-        backgroundColor: 'rgba(233, 30, 99, 0.3)',
-        borderColor:     '#E91E63',
-        pointBackgroundColor: 'rgba(255, 255, 255, 0.99)',
-        pointBorderWidth: 2,
-        pointHoverRadius: 4,
-        pointHoverBackgroundColor: 'rgba(233, 30, 99, 1)',
-        pointHoverBorderColor: 'rgba(233, 30, 99, .4)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 4,
-        pointHitRadius: 10,
-        spanGaps: false
-        // borderDash:      [5, 5],
-        // fill: false
-      }],
-    },
+  var ctx = document.getElementById('chart-graph').getContext("2d");
 
-    // Demo
-    // options: {
-    //   responsive: false,
-    //   maintainAspectRatio: false,
-    // }
-    options: {
-          animation: false,
-          legend: {display: false},
-          maintainAspectRatio: false,
-          responsive: true,
-          responsiveAnimationDuration: 0,
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true,
-                padding: 5,
-                callback: function(value, index, values) {
-                  if(parseInt(value) >= 1000){
-                    return '$' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                  } else {
-                    return '$' + value;
-                  }
-                }
-              }
-            }]
+  var data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label:           'Missing assets value',
+      data:            [226043, 220000, 250000, 160000, 210000, 219000, 280000],
+      lineTension: 0.4,
+      borderWidth: 2,
+      backgroundColor: 'rgba(233, 30, 99, 0.3)',
+      borderColor:     '#E91E63',
+      pointBackgroundColor: 'rgba(255, 255, 255, 0.99)',
+      pointBorderWidth: 2,
+      pointHoverRadius: 4,
+      pointHoverBackgroundColor: 'rgba(233, 30, 99, 1)',
+      pointHoverBorderColor: 'rgba(233, 30, 99, .4)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      spanGaps: false
+    }],
+  };
+
+  var options = {
+    animation: false,
+    legend: {display: false},
+    maintainAspectRatio: false,
+    responsive: true,
+    responsiveAnimationDuration: 0,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          padding: 5,
+          callback: function(value, index, values) {
+            if(parseInt(value) >= 1000){
+              return '$' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            } else {
+              return '$' + value;
+            }
           }
         }
+      }]
+    }
+  };
+
+  var graphChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
   });
 
   function resizeCharts() {
-      graphChart.resize();
-    }
+    graphChart.resize();
+  }
 
-    // Initial resize
-    resizeCharts();
+  // Initial resize
+  resizeCharts();
 
-    // For performance reasons resize charts on delayed resize event
-    window.layoutHelpers.on('resize.charts-demo', resizeCharts);
+  // For performance reasons resize charts on delayed resize event
+  window.layoutHelpers.on('resize.charts-demo', resizeCharts);
+
+
+  const api_url = 'http://localhost:3000/monthly';
+  async function getData() {
+    const response = await fetch(api_url);
+    const api_data = await response.json();
+    graphChart.data.datasets[0].data = api_data.datasets[0].data;
+    graphChart.data.labels = api_data.labels;
+
+    console.table([data.labels, data.datasets[0].data]);
+
+    // Outputing pretty JSON-style :
+    console.log(JSON.stringify(api_data, undefined, 2));
+
+    // Outputing compact JSON-style :
+    // console.log(JSON.stringify(api_data));
+    return api_data;
+  }
+
+  setInterval(function() {
+    getData().catch(err => {
+        console.log("Unable to get chart data from server", err);
+      });
+
+    graphChart.update();
+  }, 3500);
+
 });
